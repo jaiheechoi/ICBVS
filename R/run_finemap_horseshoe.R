83bde511483db773a3cf78f31c756a6c8e42e395
jaiheechoi <98553015+jaiheechoi@users.noreply.github.com>
2025-04-20 21:46:31 -0500

    change name

--- R/run_finemap_horseshoe.R
+++ R/run_finemap_horseshoe.R
@@ -1,5 +1,5 @@
 # run and record
-horseshoe <- function(xMat, p, q, leftTimes, rightTimes, nKnots=1, tposInd, obsInd, sigmaAprop, sigmaEprop, sigmaBprop,
+run_finemap_horseshoe <- function(xMat, p, q, leftTimes, rightTimes, nKnots=1, tposInd, obsInd, sigmaAprop, sigmaEprop, sigmaBprop,
                           sigmaAprior, sigmaEprior, sigmaBprior, A, B, subPct, quant_r=NULL, addProb, removeProb,
                           seed=0, burnIn=0.2, alphaPriorMean=NULL, etaPriorMean = NULL, checkpoint=FALSE) {
   
